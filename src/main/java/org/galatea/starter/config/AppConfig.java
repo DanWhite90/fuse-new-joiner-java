91eb54cee9383c4ff6c16b749f57331cda239357
Daniele Bianchin <bianchin.daniele.b@gmail.com>
2025-08-25 22:01:54 +0100

    fix feign clients

--- src/main/java/org/galatea/starter/config/AppConfig.java
+++ src/main/java/org/galatea/starter/config/AppConfig.java
@@ -18,7 +18,7 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 @Configuration
 @EnableAspectJAutoProxy
 @EnableCaching
-@EnableFeignClients
+@EnableFeignClients(basePackages = "org.galatea.starter.service")
 public class AppConfig {
 
   /**
